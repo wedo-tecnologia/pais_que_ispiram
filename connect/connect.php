4f64cb3206edcb9b8f26840e715e857f8c1dd05f
Fábio Adler de Luna Gomes <56208895+xrlplerl@users.noreply.github.com>
2022-08-05 10:03:18 -0300

    projeto inicial

---
+++ connect/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('host','127.0.0.1');
+    define('user','root');
+    define('senha','');
+    #nome da tabela
+    define('db','pais_que_ispiram');
+
+    $con = mysqli_connect(host,user,senha,db);
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' href='./css/config.css'>
+    <link rel='stylesheet' type='text/css' href='./css/index.css'>
+    <title>Pais que ispiram</title>
+</head>
+<body>
+    <?php
+        include('./connect/connect.php');
+    ?>
+    <div class='org_site'>
+        <div class='topo'>
+            <div class='logo_topo'>Pais que ispiram</div>
+            <div class='button_topo'><button>Criar imagem</button></div>
+        </div>
+    </div>
+</body>
+</html>
