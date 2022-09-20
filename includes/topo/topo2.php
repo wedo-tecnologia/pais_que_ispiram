8178c8fc1f03d8508dba190187ce1912308e4e40
Fábio Adler de Luna Gomes <56208895+xrlplerl@users.noreply.github.com>
2022-09-20 18:05:07 -0300

    fix topo

--- includes/topo/topo2.php
+++ includes/topo/topo2.php
@@ -1,7 +1,7 @@
 <header class="main_header">
     <div class="main_header_content">
         <a href="/" class="logo">
-            <img src="img/logo.png" alt="ISAC" title="ISAC">
+            <img src="../img/logo.png" alt="ISAC" title="ISAC">
         </a>
         <nav class="main_header_content_menu">
             <ul>
